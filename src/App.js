4c9f170fc5b7d44f5f956e6a04836338d7c4a35c
minsu <choalstn@gmail.com>
2022-07-15 21:10:13 +0900

    useEffect(3)

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
-import Button from "./Button"
-import styles from "./App.module.css"
+import { func } from "prop-types";
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  const onClick = () => setValue(counter + 1);
-  const onChange = (event) => {setKeyword(event.target.value)};
+function Hello() {
+  /**function Hello() {
+    function byFn() {
+      console.log("bye :(")
+    }
   
+    function hiFn() {
+      console.log("created :)")
+      return byFn;
+    }
+    useEffect(hiFn, [])
+    return <h1>Hello</h1>
+  }
+  **/
+ // 위 아래 동일한 결과를 나타내지만, 코드의 단순성에서 큰 차이
+  
+  useEffect(() => {
+    console.log('hi :)')
+    return () => {console.log("bye :(")}
+  }, [])
+  return <h1>Hello</h1>
+}
 
-  useEffect(() => {console.log("I run only once")}, [] )
-  useEffect(() => {
-    console.log("I run when 'keyword' changes")
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes")
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'counter & counter' changes")
-  }, [counter, keyword]);
+function App() {
+  const[showing, setShowing] = useState(false);
+
+  const onClick = () => setShowing(!showing);
+
 
   return (
     <div>
-      <input 
-      value={keyword} 
-      onChange={onChange} 
-      type="text" 
-      placeholder="Search Here..."
-       /> 
-
-      <h1 >{counter}</h1>
-      <button onClick={onClick}>Click me</button>
-      </div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
   );
 }
 
