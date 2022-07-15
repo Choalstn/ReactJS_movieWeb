cedf14b4c346ab8bfe416d2c46120efee3f08e2e
minsu <choalstn@gmail.com>
2022-07-15 21:44:54 +0900

    toDo List(1)

--- src/App.js
+++ src/App.js
@@ -1,39 +1,26 @@
-import { func } from "prop-types";
-import { useState, useEffect } from "react";
 
-function Hello() {
-  /**function Hello() {
-    function byFn() {
-      console.log("bye :(")
-    }
-  
-    function hiFn() {
-      console.log("created :)")
-      return byFn;
-    }
-    useEffect(hiFn, [])
-    return <h1>Hello</h1>
-  }
-  **/
- // 위 아래 동일한 결과를 나타내지만, 코드의 단순성에서 큰 차이
-  
-  useEffect(() => {
-    console.log('hi :)')
-    return () => {console.log("bye :(")}
-  }, [])
-  return <h1>Hello</h1>
-}
+import { useState} from "react";
 
 function App() {
-  const[showing, setShowing] = useState(false);
-
-  const onClick = () => setShowing(!showing);
-
-
-  return (
+  const [toDo, setToDo] = useState("")
+  const [toDos, setToDos] = useState([]) 
+  const onChange =(event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(toDo);
+    if(toDo == "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo('');
+  };
+  return ( 
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos {(toDos.length)}</h1>
+      <form onSubmit={onSubmit}>
+      <input onChange={onChange} value={toDo} type="text" placeholder="Write yout To Do..."/>
+      <button>Add ToDo</button>
+      </form>
     </div>
   );
 }
