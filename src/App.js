813c895327a6caa50179070b4c6108aa95c05709
minsu <choalstn@gmail.com>
2022-07-21 01:32:04 +0900

    Movie App (2)

--- src/App.js
+++ src/App.js
@@ -1,51 +1,7 @@
-import styles from "./App.module.css"
-import { useEffect, useState} from "react";
+
 
 function App() {
-  const [loading, setLoading] = useState(true); 
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async() => {
-    const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year`);
-    const json = await response.json();
-  
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies()}, []
-    );
-
-  console.log(movies)
-
-  /** useEffect(() => {
-    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year`)
-    .then((response) => response.json())
-    .then((json) => {
-      setMovies(json.data.movies)
-      setLoading(false);
-      console.log(loading)
-    }) }, []);
-    **/
-
-
-
-  return ( 
-    <div>
-      {loading ? <h1>Loading...</h1> 
-       : movies.map(movie => <div key={movie.id}>
-        <h2> {movie.title} </h2>
-        <img src={movie.medium_cover_image} />
-        <p> {movie.summary}  </p>
-        <ul>
-        {movie.genres.map((g) => (
-          <li key={g}>{g}</li>
-        ))}
-        </ul>
-        </div>)}
-    </div>
-  );
+  return null
 }
 
 export default App;
