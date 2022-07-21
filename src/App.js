96229887e4ff6d4b5487cddcfc618486237a9710
minsu <choalstn@gmail.com>
2022-07-21 21:57:42 +0900

    React Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; //HasgRouter는 뒤에 #이 붙고 path가 붙음 대개로 BrowserRouter 사용
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
+//이전에는 App이 모든 걸 다 하고 있었는데, App의 로직들을 분리시킴 
 
 function App() {
-  return null
+  return (
+    <Router> 
+      <Switch> 
+        <Route path={"/movie"}> 
+          <Detail />
+        </Route>
+
+        <Route path="/" >
+          <Home />
+        </Route>
+      </Switch> 
+    </Router> //Switch는 Router를 찾고, 찾으면 렌더링함
+  )
+  
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,15 @@
 import PropTypes from "prop-types"; 
+import {Link} from "react-router-dom";
+
+//<a> 태그 대신 Link를 사용하는 이유 : 결과는 동일하지만, <a>태그는 페이지 전체를 재로딩함
 
 function Movie({title, coverImg, summary, genres}) {
      return (
         <div>
-        <h2> {title} </h2>
         <img src={coverImg} alt={title}/>
+        <h2> 
+          <Link to="/movie">{title} </Link>    
+        </h2>
         <p> {summary}  </p>
         <ul>
         {genres.map((g) => (
